4143e8a46647afe08f17bb5d85a501fff02ccdf4
Ivan Kahl <hello@ivankahl.com>
2022-03-01 12:09:25 +0000

    Updated to top level file

--- Program.cs
+++ Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace dotnetcore
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿Console.WriteLine("Hello!");
